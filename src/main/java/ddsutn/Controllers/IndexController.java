728385b974cf199086571a7368350f3f04de4541
CarolinaSeoane <carolina.b.seoane@gmail.com>
2021-10-11 18:22:43 -0300

    Agrego pantallas y modifico controllers

--- src/main/java/ddsutn/Controllers/IndexController.java
+++ src/main/java/ddsutn/Controllers/IndexController.java
@@ -8,9 +8,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 
 	@RequestMapping("/")
-	public String mostrarPantalla(Model model) {
+	public String index(Model model) {
 		model.addAttribute("mensaje", "Estas en la página principal.");
 		return "index";
+	}
+
+	@RequestMapping("/Iniciar Sesion")
+	public String iniciarSesion() {
+		return "Iniciar_Sesion";
+	}
+
+	@RequestMapping("/Registrar Mascota")
+	public String registrarMascota() {
+		return "Registrar_Mascota";
+	}
+
+	@RequestMapping("/Adoptar")
+	public String adoptar() {
+		return "Adoptar_Mascota";
+	}
+
+	@RequestMapping("/Dar en Adopcion")
+	public String darEnAdopcion() {
+		return "Dar_En_Adopcion";
+	}
+
+	@RequestMapping("/Buscar Hogar")
+	public String buscarHogar() {
+		return "Buscar_Hogar";
+	}
+
+	@RequestMapping("/Encontre Mascota Perdida")
+	public String encontreMascotaPerdida() {
+		return "Encontre_Mascota_Perdida";
+	}
+
+	@RequestMapping("/Buscar Mascota Perdida")
+	public String buscarMascota() {
+		return "Buscar_Mascota_Perdida";
 	}
 
 }
