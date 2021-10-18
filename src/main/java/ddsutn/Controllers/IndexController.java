099badc40ca13e7e7a7878124e46b17839f413ac
CarolinaSeoane <carolina.b.seoane@gmail.com>
2021-10-18 15:38:17 -0300

    Separo logica de Adoptar a otro controller

--- src/main/java/ddsutn/Controllers/IndexController.java
+++ src/main/java/ddsutn/Controllers/IndexController.java
@@ -1,15 +1,8 @@
 package ddsutn.Controllers;
 
-import ddsutn.Business.Publicacion.PublicacionDarEnAdopcion;
-import ddsutn.Servicios.PublicacionDarEnAdopcionSvc;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 public class IndexController {
@@ -30,71 +23,9 @@ public class IndexController {
 		return "Iniciar_Sesion";
 	}
 
-
-
-	@Autowired
-	PublicacionDarEnAdopcionSvc publicacionDarEnAdopcionSvc;
-
-	@RequestMapping("/Adoptar/Publicaciones_Adopcion")
-	public String Mascotas_En_Adopción(Model model) {
-		List<PublicacionDarEnAdopcion> publicaciones =publicacionDarEnAdopcionSvc.publicaciones();
-
-		model.addAttribute("recientes",publicaciones.stream()
-				.sorted(Comparator.comparingLong( PublicacionDarEnAdopcion::getId )
-						.reversed())
-				.limit(4)
-				.collect(Collectors.toList()));
-
-		model.addAttribute("publicaciones",publicaciones.stream().limit(2).collect(Collectors.toList()));
-
-		model.addAttribute("cantidad",4);
-		model.addAttribute("ordenado","");
-		return "Publicaciones_Adopcion";
-
-	}
-	@GetMapping("/Adoptar/Publicaciones_Adopcion/{cantidad}")
-	public String cargarMas(Model model, @PathVariable int cantidad) {
-		List<PublicacionDarEnAdopcion> publicaciones =publicacionDarEnAdopcionSvc.publicaciones();
-
-		model.addAttribute("recientes",publicaciones.stream()
-				.sorted(Comparator.comparingLong( PublicacionDarEnAdopcion::getId )
-						.reversed())
-				.limit(4)
-				.collect(Collectors.toList()));
-
-		model.addAttribute("publicaciones",publicaciones.stream().limit(cantidad).collect(Collectors.toList()));
-		model.addAttribute("cantidad",cantidad+2);
-		model.addAttribute("ordenado","");
-		return "Publicaciones_Adopcion";
-	}
-	@GetMapping("/Adoptar/Publicaciones_Adopcion/{cantidad}/{ordenado}")
-	public String ordenar(Model model, @PathVariable int cantidad,@PathVariable String ordenado) {
-		List<PublicacionDarEnAdopcion> publicaciones =publicacionDarEnAdopcionSvc.publicaciones();
-
-		model.addAttribute("recientes",publicaciones.stream()
-				.sorted(Comparator.comparingLong( PublicacionDarEnAdopcion::getId )
-						.reversed())
-				.limit(4)
-				.collect(Collectors.toList()));
-
-		model.addAttribute("publicaciones",publicaciones.stream()
-						.sorted(Comparator.comparing(n->n.getMascota().getNombre()) )
-				.limit(cantidad)
-				.collect(Collectors.toList()));
-		model.addAttribute("cantidad",cantidad+2);
-		model.addAttribute("ordenado","ordenado");
-		return "Publicaciones_Adopcion";
-	}
-
-
 	@RequestMapping("/Registrar Mascota")
 	public String registrarMascota() {
 		return "Registrar_Mascota";
-	}
-
-	@RequestMapping("/Adoptar")
-	public String adoptar() {
-		return "Adoptar_Mascota";
 	}
 
 	@RequestMapping("/Dar en Adopcion")
@@ -116,7 +47,5 @@ public class IndexController {
 	public String buscarMascota() {
 		return "Buscar_Mascota_Perdida";
 	}
-
-
 
 }
