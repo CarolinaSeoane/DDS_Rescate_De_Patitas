6ffbc5f0fd8cee4069796e040955561b094249d7
FacuVerge <facu.verge@gmail.com>
2021-11-06 01:35:58 -0300

    Crear Publicacion, ahora con validaciones de Form

--- src/main/resources/static/Crear_Publicacion/Crear_Publicacion.js
+++ src/main/resources/static/Crear_Publicacion/Crear_Publicacion.js
@@ -22,7 +22,9 @@ var app1 = new Vue({
     },
     methods: {
     	enviar() {
-			axios.post(apiUrlPublicacion, this.form).then((result) => {console.log(result);})
+    		if(this.elFormEsValido()){
+    			axios.post(apiUrlPublicacion, this.form).then((result) => {console.log(result);})
+    		}
         },
         habilitar: function() {
         	let edadMin = document.getElementById("edadMin")
@@ -33,12 +35,54 @@ var app1 = new Vue({
         deshabilitar: function() {
             let edadMin = document.getElementById("edadMin")
             let edadMax = document.getElementById("edadMax")
+            edadMin.value = null 								//Esto no los borra del Form, es solo para que no se vean en pantalla
+            edadMax.value = null
             edadMin.disabled = true
             edadMax.disabled = true
         },
         validEmail: function (email) {
             var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
             return re.test(email);
+        },
+        elFormEsValido: function() {
+        	let esValido = true
+
+        	if(this.form.emailDelInteresado == null) {
+        		alert("El email es un campo obligatorio")
+        		esValido = false
+        	}else if(!this.validEmail(this.form.emailDelInteresado)) {
+        		alert("El email que escribiste no es válido. Revisa la dirección")
+        		esValido = false
+        	}
+
+        	if(this.form.preferencias.tipoMascota == null) {
+        		alert("El tipo de mascota es un campo obligatorio")
+        		esValido = false
+        	}
+
+			if(this.form.preferencias.sexo == null) {
+                alert("El sexo de la mascota es un campo obligatorio")
+                esValido = false
+            }
+
+			let entre 			= document.getElementById("entre")
+			let cualquierEdad 	= document.getElementById("cualquier-Edad")
+
+			if(!entre.checked && !cualquierEdad.checked) {
+				alert("La edad es un campo obligatorio")
+				esValido = false
+			} else if(entre.checked) {
+				if(this.form.preferencias.edadMax <= this.form.preferencias.edadMin) {
+					alert("La edad máxima no puede ser menor ni igual que la edad mínima. Revise el rango de edad")
+					esValido = false
+				}
+			} else {
+				// En BBDD se recibe como 0 y 0. Representa que eligió la opcion "Cualquiera"
+				// Si ponia rango, el form lo limita a que la edadMax sea como mínimo 1
+				this.form.preferencias.edadMax = null
+				this.form.preferencias.edadMin = null
+			}
+        	return esValido
         }
     },
 	created () {
