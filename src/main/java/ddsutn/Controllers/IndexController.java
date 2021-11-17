c973885b9bc3c1773247e5dbfe7b929f4cb3d508
pablo goncalves <goncalves.pab@gmail.com>
2021-11-17 19:29:56 -0300

    procfile

--- src/main/java/ddsutn/Controllers/IndexController.java
+++ src/main/java/ddsutn/Controllers/IndexController.java
@@ -3,13 +3,18 @@ package ddsutn.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
+@CrossOrigin
 @RestController
 public class IndexController {
 
+	//@RequestMapping("/")
+	//public String localhost(Model model) {
+		//return "redirect:/index";
+	//}
 	@RequestMapping("/")
-	public String localhost(Model model) {
-		return "redirect:/index";
+	@ResponseBody
+	String home() {
+		return "Hello World!";
 	}
 
 }
