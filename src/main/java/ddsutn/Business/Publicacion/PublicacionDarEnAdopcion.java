9f77ca7f20dd6ba94063bedd819778b684d1fa07
FacuVerge <facu.verge@gmail.com>
2021-11-29 00:37:24 -0300

    Modifico Scheduler. Se envian las notificaciones

--- src/main/java/ddsutn/Business/Publicacion/PublicacionDarEnAdopcion.java
+++ src/main/java/ddsutn/Business/Publicacion/PublicacionDarEnAdopcion.java
@@ -6,7 +6,6 @@ import ddsutn.Business.Organizacion.Organizacion;
 import ddsutn.Business.Mascota.Mascota;
 import ddsutn.Business.Persona.Duenio;
 import ddsutn.Business.Publicacion.Pregunta.PreguntaPublicacion;
-import ddsutn.Business.Persona.Contacto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -51,26 +50,10 @@ public class PublicacionDarEnAdopcion {
     entablar una conversacion por fuera de la plataforma.
      */
 
-	/* ??*/
-	public void notificarDuenioSobreInteresado(Contacto contactoDelInteresado) {
-		String mensaje = String.format("Hay un interesado en adoptar a %s!\nComunicarse con %s %s: Tel. %s - Email %s",
+	public void notificarDuenioSobreInteresado(String emailDelInteresado) {
+		String mensaje = String.format("Hay un interesado en adoptar a %s!\nComunicarse por mail a esta direccion: %s ",
 				this.mascota.getNombre(),
-				contactoDelInteresado.getNombre(),
-				contactoDelInteresado.getApellido(),
-				contactoDelInteresado.getTelefono(),
-				contactoDelInteresado.getEmail());
-
-		Duenio duenioMascota = this.mascota.getDuenio();
-		duenioMascota.notificarAMisContactos(mensaje);
-	}
-
-	public void notificarDuenioSobreInteresado(Duenio duenio) {
-		String mensaje = String.format("Hay un interesado en adoptar a %s!\nComunicarse con %s %s: Tel. %s - Email %s",
-				this.mascota.getNombre(),
-				duenio.getNombre(),
-				duenio.getApellido(),
-				duenio.getTelefono(),
-				duenio.getEmail());
+				emailDelInteresado);
 
 		Duenio duenioMascota = this.mascota.getDuenio();
 		duenioMascota.notificarAMisContactos(mensaje);
--- src/main/java/ddsutn/Servicios/PublicacionInteresadoSvc.java
+++ src/main/java/ddsutn/Servicios/PublicacionInteresadoSvc.java
@@ -1,8 +1,6 @@
 package ddsutn.Servicios;
 
-import ddsutn.Business.Persona.Duenio;
 import ddsutn.Business.Publicacion.PublicacionInteresado;
-import ddsutn.Repositorio.PersonaRepo;
 import ddsutn.Repositorio.PublicacionInteresadoRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
